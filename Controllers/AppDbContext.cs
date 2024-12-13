143bc329c12e87129ab80ec5b14391347740babe
Ahmed anwer elrweny <elrwenyahmed05@gmail.com>
2024-12-13 16:27:12 +0200

    Add project files.

---
+++ Controllers/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<Admin> Admins { get; set; }
+        DbSet<Department> Departments{ get; set; }
+        DbSet<SuperVisor> SuperVisors { get; set; }
+        DbSet<Employee> Employees { get; set; }
+        DbSet<BigTask> BigTasks{ get; set; }
+        DbSet<SubTask> SubTasks { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server = . ; DataBase = TaskManagement ; TrustServerCertificate = true ; Integrated Security =SSPI ");
+        }
+    }
+}
---
+++ Migrations/20241209215637_initial.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Project.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Adress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BigTasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartLine = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeadtLine = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Progress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Dept_Id = table.Column<int>(type: "int", nullable: false),
+                    Super_Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BigTasks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Super_Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SuperVisors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Adress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Dept_Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperVisors", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SuperVisors_Departments_Dept_Id",
+                        column: x => x.Dept_Id,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Adress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Dept_Id = table.Column<int>(type: "int", nullable: false),
+                    Super_Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Departments_Dept_Id",
+                        column: x => x.Dept_Id,
+                        principalTable: "Departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Employees_SuperVisors_Super_Id",
+                        column: x => x.Super_Id,
+                        principalTable: "SuperVisors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubTasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartLine = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeadtLine = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Progress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Emp_Id = table.Column<int>(type: "int", nullable: false),
+                    BTask_Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubTasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubTasks_BigTasks_BTask_Id",
+                        column: x => x.BTask_Id,
+                        principalTable: "BigTasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_SubTasks_Employees_Emp_Id",
+                        column: x => x.Emp_Id,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BigTasks_Dept_Id",
+                table: "BigTasks",
+                column: "Dept_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BigTasks_Super_Id",
+                table: "BigTasks",
+                column: "Super_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_Super_Id",
+                table: "Departments",
+                column: "Super_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_Dept_Id",
+                table: "Employees",
+                column: "Dept_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_Super_Id",
+                table: "Employees",
+                column: "Super_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubTasks_BTask_Id",
+                table: "SubTasks",
+                column: "BTask_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubTasks_Emp_Id",
+                table: "SubTasks",
+                column: "Emp_Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SuperVisors_Dept_Id",
+                table: "SuperVisors",
+                column: "Dept_Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BigTasks_Departments_Dept_Id",
+                table: "BigTasks",
+                column: "Dept_Id",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BigTasks_SuperVisors_Super_Id",
+                table: "BigTasks",
+                column: "Super_Id",
+                principalTable: "SuperVisors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Departments_SuperVisors_Super_Id",
+                table: "Departments",
+                column: "Super_Id",
+                principalTable: "SuperVisors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SuperVisors_Departments_Dept_Id",
+                table: "SuperVisors");
+
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "SubTasks");
+
+            migrationBuilder.DropTable(
+                name: "BigTasks");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "SuperVisors");
+        }
+    }
+}
